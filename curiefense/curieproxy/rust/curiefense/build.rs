5b12129a451f9afccbdea4f4766d5f8f03f6f81e
Simon Marechal <bartavelle@gmail.com>
2022-05-16 16:39:10 +0200

    Non-lua grasshopper module
    This patch removes the Lua grasshopper module, and replaces it with a
    new C-compatible grasshopper module that is directly used from the Rust
    code.
    
    Signed-off-by: Simon Marechal <bartavelle@gmail.com>

--- curiefense/curieproxy/rust/curiefense/build.rs
+++ curiefense/curieproxy/rust/curiefense/build.rs
@@ -1,5 +1,5 @@
 fn main() {
-  println!("cargo:rustc-link-search=native=./static");
-  println!("cargo:rustc-link-lib=dylib=grasshopper");
-  println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rustc-link-search=native=./static");
+    println!("cargo:rustc-link-lib=dylib=grasshopper");
+    println!("cargo:rerun-if-changed=build.rs");
 }
