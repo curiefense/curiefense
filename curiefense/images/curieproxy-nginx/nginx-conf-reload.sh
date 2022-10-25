8f224ad2dd3a5dfed6c77cfc2c93218d44b917a4
Curiefense Bootstrap Script <curiefense-bootstrap@reblaze.com>
2022-10-25 13:59:38 +0300

    port change and lua fix

--- curiefense/images/curieproxy-nginx/nginx-conf-reload.sh
+++ curiefense/images/curieproxy-nginx/nginx-conf-reload.sh
@@ -3,23 +3,25 @@
   target_dir="/etc/nginx/conf.d"
   temp_dir="/tmp/current-conf"
   mkdir -p ${temp_dir}
+  mkdir -p /lua/customcode
   rm -rf ${temp_dir}/*
   cp -a ${target_dir}/. ${temp_dir}/
   rm -rf ${target_dir}/*
-  logger "Extract $source_file into $target_dir"
+  echo "Extract $source_file into $target_dir"
   tar xzf ${source_file} -C ${target_dir}
   ## test nginx conf
-  logger "Test Nginx"
+  echo "Test Nginx"
   nginx -t
   ## check exit code of previous command
   retVal=$?
   if [ $retVal -ne 0 ];
   then
-    logger "Nginx failed, restore config files"
+    echo "Nginx failed, restore config files"
     ## restore previously backed-up files
     cp -a  ${temp_dir}/. ${target_dir}/
+    cp ${targer_dir}/lua/customcode.lua /lua/customcode/lua.lua
   else
     ## reload nginx
     nginx -s reload
-    logger "Nginx reloaded with the new config"
+    echo "Nginx reloaded with the new config"
   fi
