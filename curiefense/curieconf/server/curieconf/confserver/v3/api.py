3cc95e7e4e2ac212f3344863450d74e03e024ad2
Evgeniy Leybovich <freakachoo.corp@gmail.com>
2023-05-15 17:40:54 +0300

    rename to graphql_path

--- curiefense/curieconf/server/curieconf/confserver/v3/api.py
+++ curiefense/curieconf/server/curieconf/confserver/v3/api.py
@@ -114,7 +114,7 @@ class ContentFilterProfile(BaseModel):
     allsections: typing.Any
     decoding: typing.Any
     masking_seed: StrictStr
-    graphql_property: Optional[StrictStr]
+    graphql_path: Optional[StrictStr]
     content_type: Optional[List[StrictStr]]
     active: Optional[List[StrictStr]]
     report: Optional[List[StrictStr]]
