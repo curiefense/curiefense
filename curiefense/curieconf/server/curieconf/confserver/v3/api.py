673accbd2ef791271c7f280dd79464b4f867a445
Curiefense Bootstrap Script <curiefense-bootstrap@reblaze.com>
2023-06-05 15:38:16 +0300

    change backup names

--- curiefense/curieconf/server/curieconf/confserver/v3/api.py
+++ curiefense/curieconf/server/curieconf/confserver/v3/api.py
@@ -1152,8 +1152,10 @@ async def backup_create(
             status.append({"name": bucket["name"], "ok": s, "logs": logs, "message": msg})
 
             print(f"start sleep for 1 minute")
+            status.append(f"start sleep for 1 minute")
             import asyncio
             await asyncio.sleep(60_000)
+            status.append(f"finish sleep for 1 minute")
             print(f"Finish sleep for 1 minute")
 
             os.remove(current_backup_filename)
